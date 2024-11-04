11b94a742c1fd3d8b6cc8ea14c162498a43f8275
satishdamo <satish.j.damodaran@gmail.com>
2024-11-05 02:56:40 +0530

    changes in app

--- app.py
+++ app.py
@@ -59,4 +59,4 @@ def predict():
     return jsonify({'label': show_class_name(int(label))})
 
 if __name__ == '__main__':
-    app.run(debug=True,port=5000)
+    app.run(host='0.0.0.0',debug=True,port=5000)
